f48a3bc2e530776d6f7e8fd3e3221076d69cd249
GuoYu Yang <49673553+pilgrimygy@users.noreply.github.com>
2021-08-26 22:56:31 +0800

    update

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,7 @@ recursive_replace(@__DIR__, r"\.zh(?<ext>(\.md)?)$" => s"\g<ext>")
 makedocs(
     format = Documenter.HTML(
         prettyurls = true,
-        analytics = "UA-132847825-3",
+        analytics = "G-5NBJ9X617X",
         lang = "zh-CN",
         footer = "本站基于 [Documenter.jl](https://github.com/JuliaDocs/Documenter.jl) 和 [Julia 编程语言](https://julialang.org/) 构建，所有内容默认遵循[CC BY 4.0](https://creativecommons.org/licenses/by/4.0/)协议。",
         assets = [
